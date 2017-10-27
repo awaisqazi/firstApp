5cc738107d1f4f6e97dee36330b8df9dac0c4649
Antonio Mistretta <ymsimei@gmail.com>
2017-10-27 19:42:31 -0400

    Initial Commit

---
+++ Awais/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Awais
+//
+//  Created by Antonio Mistretta on 10/27/17.
+//  Copyright © 2017 Antonio Mistretta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
