7b09b8a45d8a7a1f98bfe210182528237d3e89f7
awaisqazi <aaq6@njit.edu>
2017-10-27 19:55:28 -0400

    Update ViewController.swift

--- Awais/ViewController.swift
+++ Awais/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
